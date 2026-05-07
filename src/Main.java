881206cc847d0bd2b48e896d429a228ecb8dd4e2
Azarin Maxim <azarin.m88@gmail.com>
2026-05-08 01:23:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int price = 124325034;
+        short mile = 20;
+        int bonus = price/mile;
+        System.out.println("Количество начисленных миль за купленный билет: " + bonus);
+    }
+}
