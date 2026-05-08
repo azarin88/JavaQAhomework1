f2a692efb4311ca9eb8b7304045d0b688daceac5
Azarin Maxim <azarin.m88@gmail.com>
2026-05-08 13:24:04 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         int price = 124325034;
         short mile = 20;
-        int bonus = price/mile;
+        int bonus = price / mile;
         System.out.println("Количество начисленных миль за купленный билет: " + bonus);
     }
 }
